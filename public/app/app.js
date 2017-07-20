56fb724887ae6f7b4f008f4a0c25dd69370f55c0
simaoribeiro27 <simaoo_arp@hotmail.com>
2017-07-20 23:49:15 +0100

    trabalho android

---
+++ public/app/app.js
@@ -0,0 +1,241 @@
+angular.module('ViagensRESTWebsite', ['angular-storage', 'ui.router', 'weblogng'])
+    .constant('ENDPOINT_URI', 'http://localhost:3000/')
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/templates/login.tmpl.html',
+                controller: 'LoginCtrl',
+                controllerAs: 'login'
+            })
+            .state('dashboard', {
+                url: '/viagens',
+                templateUrl: 'app/templates/dashboard.tmpl.html',
+                controller: 'DashboardCtrl',
+                controllerAs: 'dashboard'
+            });
+
+        $urlRouterProvider.otherwise('/viagens');
+
+        $httpProvider.interceptors.push('APIInterceptor');
+    })
+    .service('APIInterceptor', function($rootScope, UserService) {
+        var service = this;
+
+        service.request = function(config) {
+            var currentUser = UserService.getCurrentUser(),
+                access_token = currentUser ? currentUser.access_token : null;
+
+            if (access_token) {
+                config.headers.authorization = access_token;
+            }
+            return config;
+        };
+
+        service.responseError = function(response) {
+            if (response.status === 401) {
+                $rootScope.$broadcast('unauthorized');
+            }
+            return response;
+        };
+    })
+    .service('UserService', function(store) {
+        var service = this,
+            currentUser = null;
+
+        service.setCurrentUser = function(user) {
+            currentUser = user;
+            store.set('user', user);
+            return currentUser;
+        };
+
+        service.getCurrentUser = function() {
+            if (!currentUser) {
+                currentUser = store.get('user');
+            }
+            return currentUser;
+        };
+    })
+    .service('LoginService', function($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'Users/';
+
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
+
+        function getLogUrl(action) {
+            return getUrl() + action;
+        }
+
+        service.login = function(credentials) {
+            return $http.post(getLogUrl('login'), credentials);
+        };
+
+        service.logout = function() {
+            return $http.post(getLogUrl('logout'));
+        };
+
+        service.register = function(user) {
+            return $http.post(getUrl(), user);
+        };
+    })
+    .service('ItemsModel', function ($http, ENDPOINT_URI) {
+        var service = this,
+            path = 'viagens';
+
+        function getUrl() {
+            return ENDPOINT_URI + path;
+        }
+
+        function getUrlForId(itemId) {
+            return getUrl(path) + itemId;
+        }
+
+        service.all = function () {
+            return $http.get(getUrl());
+        };
+
+        service.fetch = function (itemId) {
+            return $http.get(getUrlForId(itemId));
+        };
+
+        service.create = function (item) {
+            return $http.post(getUrl(), item);
+        };
+
+        service.update = function (itemId, item) {
+            return $http.put(getUrlForId(itemId), item);
+        };
+
+        service.destroy = function (itemId) {
+            return $http.delete(getUrlForId(itemId));
+        };
+    })
+    .controller('LoginCtrl', function($rootScope, $state, LoginService, UserService){
+        var login = this;
+
+        function signIn(user) {
+            LoginService.login(user)
+                .then(function(response) {
+                    user.access_token = response.data.id;
+                    UserService.setCurrentUser(user);
+                    $rootScope.$broadcast('authorized');
+                    $state.go('dashboard');
+                });
+        }
+
+        function register(user) {
+            LoginService.register(user)
+                .then(function(response) {
+                    login(user);
+                });
+        }
+
+        function submit(user) {
+            login.newUser ? register(user) : signIn(user);
+        }
+
+        login.newUser = false;
+        login.submit = submit;
+    })
+    .controller('MainCtrl', function ($rootScope, $state, LoginService, UserService) {
+        var main = this;
+
+        function logout() {
+            LoginService.logout()
+                .then(function(response) {
+                    main.currentUser = UserService.setCurrentUser(null);
+                    $state.go('login');
+                }, function(error) {
+                    console.log(error);
+                });
+        }
+
+        $rootScope.$on('authorized', function() {
+            main.currentUser = UserService.getCurrentUser();
+        });
+
+        $rootScope.$on('unauthorized', function() {
+            main.currentUser = UserService.setCurrentUser(null);
+            $state.go('login');
+        });
+
+        main.logout = logout;
+        main.currentUser = UserService.getCurrentUser();
+    })
+    .controller('DashboardCtrl', function(ItemsModel){
+        var dashboard = this;
+
+        function getItems() {
+            ItemsModel.all()
+                .then(function (result) {
+                    dashboard.items = result.data;
+                });
+        }
+
+        function createItem(item) {
+            ItemsModel.create(item)
+                .then(function (result) {
+                    initCreateForm();
+                    getItems();
+                });
+        }
+
+        function updateItem(item) {
+            ItemsModel.update(item.id, item)
+                .then(function (result) {
+                    cancelEditing();
+                    getItems();
+                });
+        }
+
+        function deleteItem(itemId) {
+            ItemsModel.destroy(itemId)
+                .then(function (result) {
+                    cancelEditing();
+                    getItems();
+                });
+        }
+
+        function initCreateForm() {
+            dashboard.newItem = { name: '', description: '' };
+        }
+
+        function setEditedItem(item) {
+            dashboard.editedItem = angular.copy(item);
+            dashboard.isEditing = true;
+        }
+
+        function isCurrentItem(itemId) {
+            return dashboard.editedItem !== null && dashboard.editedItem.id === itemId;
+        }
+
+        function cancelEditing() {
+            dashboard.editedItem = null;
+            dashboard.isEditing = false;
+        }
+
+        dashboard.items = [];
+        dashboard.editedItem = null;
+        dashboard.isEditing = false;
+        dashboard.getItems = getItems;
+        dashboard.createItem = createItem;
+        dashboard.updateItem = updateItem;
+        dashboard.deleteItem = deleteItem;
+        dashboard.setEditedItem = setEditedItem;
+        dashboard.isCurrentItem = isCurrentItem;
+        dashboard.cancelEditing = cancelEditing;
+
+        initCreateForm();
+        getItems();
+    })
+  .constant('weblogngConfig', {
+    apiKey: '48548598-f079-4c57-bb39-d9ca8344abd7',
+    options: {
+      publishNavigationTimingMetrics: true,
+      publishUserActive: true,
+      application: 'simple-rest-website'
+    }
+  })
+;
